ae06cd175b7599f74cace9f4cf07f087bd99b6d5
jxrvll <jxrvll@gmail.com>
2020-09-02 12:30:19 -0400

    add handlebars and search feature

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,29 +1,46 @@
-// Requiring path to so we can use relative routes to our HTML files
-const path = require("path");
-
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
+    const memberObject = {
+      user: req.user
+    };
     if (req.user) {
-      res.redirect("/members");
+      // res.redirect("/members");
+      res.render("members", memberObject);
+    } else {
+      res.render("signup");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    // res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   app.get("/login", (req, res) => {
+    const memberObject = {
+      user: req.user
+    };
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      // res.redirect("/members");
+      res.render("members", memberObject);
+    } else {
+      // res.sendFile(path.join(__dirname, "../public/login.html"));
+      res.render("login");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    // res.sendFile(path.join(__dirname, "../public/members.html"));
+    const memberObject = {
+      email: req.user.email,
+      city: req.user.city,
+      state: req.user.state,
+      zip: req.user.zip
+    };
+    // console.log(req.user);
+    res.render("members", memberObject);
   });
 };
